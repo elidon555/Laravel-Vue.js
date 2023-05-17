ab2ed333628b4f10dbce1049fdb98866b293bbd5
elidon555 <elidonneziri@gmail.com>
2023-05-17 22:34:39 +0200

    changes

--- _vue/src/views/Subscribe/PlanCard.vue
+++ _vue/src/views/Subscribe/PlanCard.vue
@@ -75,38 +75,38 @@ const props = defineProps({
 <!--    <div class="max-w-sm rounded overflow-hidden shadow-lg text-center bg-dark m-2">-->
 <!--    </div>-->
 
-    <v-card class="max-w-md m-auto lg:m-0"
-
-
-    >
-        <div class="m-3">
-            <div class="text-center text-lg font-bold">
-              {{ title }}
-            </div>
-            <br>
-            <v-img
-                class="mx-8"
-                :src="'https://picsum.photos/500/300?image='+Math.floor(Math.random() * 100)"
-                cover
-            ></v-img>
-
-            <div class="text-center text-2xl font-semibold my-4">
-                $1{{amount}} / {{duration}}
-            </div>
-            <v-card-actions>
-                <v-btn
-                    class="w-full"
-                    color="blue"
-                    variant="elevated"
-                >
-                    Join
-                </v-btn>
-
-            </v-card-actions>
-
-          <v-card-text v-html="description"/>
-
+  <div class="p-3 ">
+    <v-card class="max-w-md m-auto lg:m-0 ">
+      <div class="m-1 bg-gradient-to-tl from-gray-700 via-gray-900 to-black">
+        <div class="text-center text-lg font-bold pt-3">
+          {{ title }}
         </div>
+        <br>
+        <v-img
+            class="mx-8 rounded-lg"
+            :src="'https://picsum.photos/500/300?image='+Math.floor(Math.random() * 100)"
+            cover
+        ></v-img>
+
+        <div class="text-center text-2xl font-semibold my-4">
+          $1{{amount}} / {{duration}}
+        </div>
+        <v-card-actions>
+          <v-btn
+              class="w-full"
+              color="blue"
+              variant="elevated"
+          >
+            Join
+          </v-btn>
+
+        </v-card-actions>
+
+        <v-card-text v-html="description"/>
+
+      </div>
 
     </v-card>
+  </div>
+
 </template>
--- _vue/src/views/Subscribe/PlanView.vue
+++ _vue/src/views/Subscribe/PlanView.vue
@@ -1,58 +1,56 @@
 <template>
-    <div v-if="subscriptionPlans.yearly.length || subscriptionPlans.monthly.length" class="mx-auto">
-        <h1 class="text-center text-2xl font-bold my-4 tracking-wider">
-            <br>
-            Select a membership level
-        </h1>
-        <div class="justify-center max-w-6xl m-auto">
+  <div v-if="subscriptionPlans.yearly.length || subscriptionPlans.monthly.length" class="mx-auto">
+    <h1 class="text-center text-2xl font-bold my-4 tracking-wider">
+      <br>
+      Select a membership level
+    </h1>
+    <div class="justify-center max-w-6xl m-auto">
 
-            <v-tabs
-                v-model="tab"
-                color="deep-purple-accent-4"
-                align-tabs="center"
-            >
-                <v-tab v-if="subscriptionPlans.monthly.length" @click="showYearly=false;showMonthly=true" value="monthly">Monthly</v-tab>
-                <v-tab  v-if="subscriptionPlans.yearly.length" @click="showMonthly=false;showYearly=true"  value="yearly">Yearly</v-tab>
-            </v-tabs>
-            <v-divider class="m-3"></v-divider>
-            <v-window v-model="tab" class="w-full" >
-                <v-window-item value="monthly">
-                    <Transition>
-                        <div v-if="showMonthly" class="m-2">
-                            <v-row class="mx-4">
-                                <v-col cols="12" sm="12" md="4" lg="4" v-for="plan in subscriptionPlans.monthly">
-
-                                    <PlanCard
-                                        :title="plan.name"
-                                        :amount="parseInt(plan.price)"
-                                        :description="plan.features"
-                                        :duration="plan.interval"
-                                    />
-                                </v-col>
-
-                            </v-row>
-                        </div>
-                    </Transition>
-                </v-window-item>
-                <v-window-item value="yearly">
-                    <Transition>
-                        <div v-if="showYearly" class="flex">
-                            <div v-for="plan in subscriptionPlans.yearly">
-                                <PlanCard
-                                    :title="plan.name"
-                                    :amount="parseInt(plan.price)"
-                                    :icon-1="plan.features.split(' | ')[0]"
-                                    :icon-2="plan.features.split(' | ')[1]"
-                                    :duration="plan.interval"
-                                    @click="createPaymentIntent(plan)"
-                                />
-                            </div>
-                        </div>
-                    </Transition>
-                </v-window-item>
-            </v-window>
-        </div>
+      <v-tabs
+          v-model="tab"
+          color="deep-purple-accent-4"
+          align-tabs="center"
+      >
+        <v-tab v-if="subscriptionPlans.monthly.length" @click="showYearly=false;showMonthly=true" value="monthly">Monthly</v-tab>
+        <v-tab  v-if="subscriptionPlans.yearly.length" @click="showMonthly=false;showYearly=true"  value="yearly">Yearly</v-tab>
+      </v-tabs>
+      <v-divider class="m-3"></v-divider>
+      <v-window v-model="tab" class="w-full" >
+        <v-window-item value="monthly">
+          <Transition>
+            <div v-if="showMonthly" class="m-2">
+              <div class="mx-4 flex flex-row flex-wrap">
+                <PlanCard
+                    v-for="plan in subscriptionPlans.monthly"
+                    class="basis-full md:basis-1/3"
+                    :title="plan.name"
+                    :amount="parseInt(plan.price)"
+                    :description="plan.features"
+                    :duration="plan.interval"
+                />
+              </div>
+            </div>
+          </Transition>
+        </v-window-item>
+        <v-window-item value="yearly">
+          <Transition>
+            <div v-if="showYearly" class="flex">
+              <div v-for="plan in subscriptionPlans.yearly">
+                <PlanCard
+                    :title="plan.name"
+                    :amount="parseInt(plan.price)"
+                    :icon-1="plan.features.split(' | ')[0]"
+                    :icon-2="plan.features.split(' | ')[1]"
+                    :duration="plan.interval"
+                    @click="createPaymentIntent(plan)"
+                />
+              </div>
+            </div>
+          </Transition>
+        </v-window-item>
+      </v-window>
     </div>
+  </div>
   <br><br>
   <v-divider class="border-opacity-25"></v-divider>
 </template>
@@ -76,34 +74,34 @@ const userId = computed(() => route.params.id)
 const subscriptionPlans = computed(()=>store.state.subscriptionPlans)
 
 async function createPaymentIntent(plan) {
-    if (store.state.user.data.id === undefined) {
-        await router.push({name: 'login',params:{user_id:userId.value}})
-        return;
-    }
+  if (store.state.user.data.id === undefined) {
+    await router.push({name: 'login',params:{user_id:userId.value}})
+    return;
+  }
 
-    store.dispatch('createPaymentIntent')
-        .then(response => {
-            store.commit('setPlan', [plan.name,plan.price,plan.price_id])
-            store.commit('setStripeClientSecret',[response.data])
-        })
-        .catch(err => {
-            // debugger;
-        })
+  store.dispatch('createPaymentIntent')
+      .then(response => {
+        store.commit('setPlan', [plan.name,plan.price,plan.price_id])
+        store.commit('setStripeClientSecret',[response.data])
+      })
+      .catch(err => {
+        // debugger;
+      })
 }
 
 function getSubscriptionPlansData(url = null) {
-    store.dispatch("getSubscriptionPlans", {
-        url,
-        search: '',
-        per_page: '',
-        sort_field: 'price',
-        sort_direction: 'asc',
-        id:userId.value
-    }).then(function(response){
-    });
+  store.dispatch("getSubscriptionPlans", {
+    url,
+    search: '',
+    per_page: '',
+    sort_field: 'price',
+    sort_direction: 'asc',
+    id:userId.value
+  }).then(function(response){
+  });
 }
 onMounted(()=>{
-    getSubscriptionPlansData()
+  getSubscriptionPlansData()
 })
 
 </script>
@@ -111,11 +109,11 @@ onMounted(()=>{
 <style scoped>
 .v-enter-active,
 .v-leave-active {
-    transition: opacity 1s ease;
+  transition: opacity 1s ease;
 }
 
 .v-enter-from,
 .v-leave-to {
-    opacity: 0;
+  opacity: 0;
 }
 </style>
