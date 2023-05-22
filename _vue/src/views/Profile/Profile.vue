0a2c861f268dfe5613ab618f9924626066897a1e
elidon555 <elidonneziri@gmail.com>
2023-05-22 10:16:44 +0200

    changes

--- _vue/src/views/Profile/Profile.vue
+++ _vue/src/views/Profile/Profile.vue
@@ -4,7 +4,6 @@
 </template>
 <script>
 //importing bootstrap 5 Modules
-import "bootstrap/dist/css/bootstrap.min.css";
 import SignUpForm from "../Subscribe/SignUpForm.vue";
 export default {
     components: {SignUpForm}
